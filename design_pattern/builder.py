5781eecd8acdcb793df29c262b5364fc38c0c089
shin-hama <htysivista@gmail.com>
2021-03-11 23:05:28 +0900

    MAINT: Add some comments and use list

--- design_pattern/builder.py
+++ design_pattern/builder.py
@@ -1,3 +1,10 @@
+""" Builder Pattern
+
+複雑な工程を実施するクラスを用意して、過程を隠蔽する
+複雑な処理自体もインターフェースで実装を隠蔽することで、インターフェースを実装したもの
+であればどんなものでも再利用可能にする
+"""
+
 from abc import ABC, abstractmethod
 import sys
 
@@ -25,6 +32,8 @@ class Director(object):
         self.builder = builder
 
     def construct(self) -> None:
+        """ Use only Builder interface
+        """
         self.builder.make_title("Greeting")
         self.builder.make_string("from morning to noon")
         self.builder.make_items([
@@ -42,51 +51,51 @@ class Director(object):
 
 class TextBuilder(Builder):
     def __init__(self):
-        self._buffer = ""
+        self._buffer = []
 
     def make_title(self, title: str) -> None:
-        self._buffer += f"{'='*20}\n"
-        self._buffer += f"[ {title} ]\n"
-        self._buffer += "\n"
+        self._buffer.append(f"{'='*20}\n")
+        self._buffer.append(f"[ {title} ]\n")
+        self._buffer.append("\n")
 
     def make_string(self, string: str) -> None:
-        self._buffer += f"> {string}\n"
-        self._buffer += "\n"
+        self._buffer.append(f"> {string}\n")
+        self._buffer.append("\n")
 
     def make_items(self, items: list[str]) -> None:
         for item in items:
-            self._buffer += (f"  - {item}\n")
-        self._buffer += "\n"
+            self._buffer.append(f"  - {item}\n")
+        self._buffer.append("\n")
 
     def close(self) -> None:
-        self._buffer += f"{'='*20}\n"
+        self._buffer.append(f"{'='*20}\n")
 
     def get_result(self):
-        return self._buffer
+        return "".join(self._buffer)
 
 
 class HTMLBuilder(Builder):
     def __init__(self):
-        self._buffer = ""
+        self._buffer = []
 
     def make_title(self, title: str) -> None:
-        self._buffer += f"<html><head><title>{title}</title></head><body>"
-        self._buffer += f"<h1>{title}</h1>"
+        self._buffer.append(f"<html><head><title>{title}</title></head><body>")
+        self._buffer.append(f"<h1>{title}</h1>")
 
     def make_string(self, string: str) -> None:
-        self._buffer += f"<p>{string}</p>"
+        self._buffer.append(f"<p>{string}</p>")
 
     def make_items(self, items: list[str]) -> None:
-        self._buffer += "<ul>"
+        self._buffer.append("<ul>")
         for item in items:
-            self._buffer += (f"<li>{item}</li>")
-        self._buffer += "</ul>"
+            self._buffer.append(f"<li>{item}</li>")
+        self._buffer.append("</ul>")
 
     def close(self) -> None:
-        self._buffer += "</body></html>"
+        self._buffer.append("</body></html>")
 
     def get_result(self):
-        return self._buffer
+        return "".join(self._buffer)
 
 
 if __name__ == "__main__":
