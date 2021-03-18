2a2b88bffa5892dd68e5a9aca2b738b4ee5da09c
shin-hama <htysivista@gmail.com>
2021-03-19 00:20:33 +0900

    FEAT: Implement strategy interface

--- design_pattern/strategy.py
+++ design_pattern/strategy.py
@@ -2,6 +2,7 @@
 """
 from __future__ import annotations
 from abc import ABC, abstractmethod
+import random
 
 
 class Hand(object):
@@ -44,3 +45,57 @@ class Strategy(ABC):
     @abstractmethod
     def study(self, win: bool) -> None:
         pass
+
+
+class WinningStrategy(Strategy):
+    def __init__(self):
+        self.won = False
+        self.previous_hand: Hand = None
+
+    def next_hand(self) -> Hand:
+        if not self.won:
+            self.previous_hand = Hand.get_hand(random.randint(0, 2))
+
+        return self.previous_hand
+
+    def study(self, win: bool) -> None:
+        self.won = win
+
+
+class ProbStrategy(Strategy):
+    def __init__(self):
+        self.history = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+        self.prev_hand = 0
+        self.current_hand = 0
+
+    def next_hand(self) -> Hand:
+        bet: int = random.randint(0, self.get_sum(self.current_hand))
+        handvalue = 0
+        if bet < self.history[self.current_hand][0]:
+            handvalue = 0
+        elif (bet < self.history[self.current_hand][0] +
+              self.history[self.current_hand][1]):
+            handvalue = 1
+        else:
+            handvalue = 2
+        self.prev_hand = self.current_hand
+        self.current_hand = handvalue
+        return Hand.get_hand(handvalue)
+
+    def get_sum(self, handvalue: int) -> int:
+        _sum = 0
+        for v in self.history[handvalue]:
+            _sum += v
+
+        return _sum
+
+    def study(self, win: bool) -> None:
+        if win:
+            self.history[self.prev_hand][self.current_hand] += 1
+        else:
+            self.history[self.prev_hand][(self.current_hand + 1) % 3] += 1
+            self.history[self.prev_hand][(self.current_hand + 2) % 3] += 1
